b44e3c6af5faa8fd0e74f90f344327ab3bff4ecb
Nico Zhu <v-zhumin@microsoft.com>
2017-09-28 18:10:31 +0800

    edit same

--- PrintAliment/MainPage.xaml.cs
+++ PrintAliment/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Graphics.Printing;
@@ -16,6 +17,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Printing;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -33,7 +35,63 @@ namespace PrintAliment
         {
             this.InitializeComponent(); 
             printmgr.PrintTaskRequested += Printmgr_PrintTaskRequested;
+            MyWebView.LoadCompleted += MyWebView_LoadCompleted;
         }
+
+        async void MyWebView_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            MyWebViewRectangle.Fill = await GetWebViewBrush(MyWebView);
+            //  MyPrintPages.ItemsSource = await GetWebPages(MyWebView, new Windows.Foundation.Size(100d, 150d));
+
+        }
+
+        async Task<WebViewBrush> GetWebViewBrush(Windows.UI.Xaml.Controls.WebView webView)
+        {
+            // resize width to content
+            var _OriginalWidth = webView.Width;
+            var _WidthString = await webView.InvokeScriptAsync("eval",
+                new[] { "document.body.scrollWidth.toString()" });
+            int _ContentWidth;
+
+            if (!int.TryParse(_WidthString, out _ContentWidth))
+                throw new Exception(string.Format("failure/width:{0}", _WidthString));
+
+            webView.Width = _ContentWidth;
+
+            // resize height to content
+            var _OriginalHeight = webView.Height;
+
+            var _HeightString = await webView.InvokeScriptAsync("eval",
+                new[] { "document.body.scrollHeight.toString()" });
+            int _ContentHeight;
+
+            if (!int.TryParse(_HeightString, out _ContentHeight))
+                throw new Exception(string.Format("failure/height:{0}", _HeightString));
+
+            webView.Height = _ContentHeight;
+
+            // create brush
+            var _OriginalVisibilty = webView.Visibility;
+
+            webView.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+            var _Brush = new WebViewBrush
+            {
+                Stretch = Stretch.Uniform,
+                SourceName = webView.Name
+            };
+
+
+            _Brush.Redraw();
+
+
+            // webView.Width = _OriginalWidth;
+            //webView.Height = _OriginalHeight;
+            webView.Visibility = _OriginalVisibilty;
+            return _Brush;
+        }
+
+
         private void Printmgr_PrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs args)
         {  
             var deferral = args.Request.GetDeferral(); 
@@ -77,15 +135,16 @@ namespace PrintAliment
             bool showPrint = await PrintManager.ShowPrintUIAsync();
         }     
         private void PrintDic_AddPages(object sender, AddPagesEventArgs e)
-        { 
-            printDoc.AddPage(this);
+        {
+            Rectangle page = (Rectangle)this.FindName("MyWebViewRectangle");
+            printDoc.AddPage(page);
             printDoc.AddPagesComplete();
         }       
         private void PrintDic_Paginate(object sender, PaginateEventArgs e)
         {
             PrintTaskOptions opt = task.Options;
             PrintTaskOptionDetails printDetailedOptions = PrintTaskOptionDetails.GetFromPrintTaskOptions(e.PrintTaskOptions);
-         
+            
             printDoc.SetPreviewPageCount(1, PreviewPageCountType.Final);
         }
  
